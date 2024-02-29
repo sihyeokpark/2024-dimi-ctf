2de9b93b758d0073e66b9d58e437544d09ef62a9
BuildTools <unconfigured@null.spigotmc.org>
2024-02-29 20:26:47 +0900

    setting

--- start.sh
+++ start.sh
@@ -5,3 +5,8 @@ docker run -dp 5001:3000 you_are_l_easy
 docker run -dp 5002:13000 ssti
 
 docker run -dp 5003:13370 unreliable
+docker run -dp 5004:13371 casino
+docker run -dp 5005:13372 mic_check
+docker run -dp 5006:13373 casear
+docker run -dp 5007:13374 wt
+docker run -dp 5008:13375 exp2
