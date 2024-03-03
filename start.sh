3ff1b9b3e1bd38164d00ac3743648ad1df44de9d
sihyeokpark <exon.indiv@gmail.com>
2024-03-03 14:26:52 +0900

    feat: update start.sh

--- start.sh
+++ start.sh
@@ -10,3 +10,9 @@ docker run -dp 5005:13372 mic_check
 docker run -dp 5006:13373 casear
 docker run -dp 5007:13374 wt
 docker run -dp 5008:13375 exp2
+
+docker run -dp 5009:3000 being_admin_is_easy
+docker run -dp 5010:3001 cookie_from_lilpa
+docker run -dp 5011:60 bof_tut
+docker run -dp 5012:60 this_can_do_everything
+docker run -dp 5013:60 treasure_hunter
