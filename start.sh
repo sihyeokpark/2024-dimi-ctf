1bdcc9182a25004e54555935f8bbd0d85e53b80e
sihyeokpark <exon.indiv@gmail.com>
2024-03-14 08:58:01 +0900

    a

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 docker run -dp 5000:8000 you_are_l_medium
 docker run -dp 5001:3000 you_are_l_easy
-docker run -dp 5002:13000 ssti
+docker run -dp 5002:13000 --restart always ssti
 
 docker run -dp 5003:13370 unreliable
 docker run -dp 5004:13371 casino
